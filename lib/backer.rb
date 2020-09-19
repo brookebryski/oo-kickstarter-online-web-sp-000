87b45481af8a378d092f8c5741f652e9f6d75803
chic-state-0895 <brookeabryski@gmail.com>
2020-09-19 21:29:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
-
+  attr_accessor :name
   attr_reader :backed_projects
 
-  def initialize
+  def initialize(name)
     @backed_projects = []
+    @name = name
   end
 
   def backed_project(project)
